0652b244c137af9dda851235a3565cdf4a11066c
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2021-09-22 12:45:44 +0300

    feat: pageHeader mobile styles

--- src/blocks/burger/burger.js
+++ src/blocks/burger/burger.js
@@ -1,1 +1,11 @@
-console.log(`hullo from burger`);
+const header = document.querySelector(`.pageHeader`);
+const burger = header.querySelector(`.burger`);
+const burgerElement = header.querySelector(`.burger__element`);
+const nav = header.querySelector(`.mainNav`);
+
+const toggleNav = () => {
+  burgerElement.classList.toggle(`burger__element--close`);
+  nav.classList.toggle(`mainNav--open`);
+};
+
+burger.addEventListener(`click`, toggleNav);
