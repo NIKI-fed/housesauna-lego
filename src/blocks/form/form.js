0d6b9c6e0d3e160787bd2e91c03dec924f2ead06
NIKI-fed <nikifedorov@mail.ru>
2025-07-04 12:49:07 +0300

    fix(form): устранил баг в формах с наложением плейсхолдера на текст

--- src/blocks/form/form.js
+++ src/blocks/form/form.js
@@ -3,13 +3,13 @@ const inputs = form.querySelectorAll(`.form__input`);
 const placeholders = form.querySelectorAll(`.form__placeholder`);
 
 const inputOnChange = (evt) => {
-  if (evt.target.value != ``) {
-    evt.target.nextElementSibling.classList.toggle(`form__placeholder--top`);
-  } else {
-    evt.target.nextElementSibling.classList.toggle(`form__placeholder--top`);
+  if (evt.target.value !== ``) {
+    evt.target.nextElementSibling.classList.add(`form__placeholder--top`);
+    } else {
+    evt.target.nextElementSibling.classList.remove('form__placeholder--top');
   }
 };
 
 inputs.forEach((input) => {
-  input.addEventListener(`change`, inputOnChange);
+  input.addEventListener(`input`, inputOnChange);
 })
